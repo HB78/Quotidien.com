7c7ac2e5b8d9cf64408dda78e8df6583445f5d75
unknown <boubttahicham@gmail.com>
2025-05-30 20:44:43 +0200

    update des open graph

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,11 +4,32 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "La Revue de Presse - Pierre Jovanovic",
-  description: "Revue de presse internationale économique depuis 2008",
+  title: "Pierre Jovanovic - Revue de Presse",
+  description:
+    "Analyses expertes et décryptages quotidiens sur l'économie, la finance et les marchés mondiaux par Pierre Jovanovic",
+  openGraph: {
+    title: "Pierre Jovanovic - Revue de Presse",
+    description:
+      "Analyses expertes et décryptages quotidiens sur l'économie, la finance et les marchés mondiaux par Pierre Jovanovic",
+    images: [
+      {
+        url: "/jovanovics.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Pierre Jovanovic",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Pierre Jovanovic - Revue de Presse",
+    description:
+      "Analyses expertes et décryptages quotidiens sur l'économie, la finance et les marchés mondiaux par Pierre Jovanovic",
+    images: ["/jovanovics.jpg"],
+  },
 };
 
-export default function JovanovicLayout({ children }) {
+export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <body className={inter.className}>{children}</body>
